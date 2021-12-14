afed84730502795b7992db894255c749497f86d0
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-14 15:46:18 +0200

    Implementing the pipeline stages

--- src/SG.EnumGenerators/EnumGenerator.cs
+++ src/SG.EnumGenerators/EnumGenerator.cs
@@ -31,4 +31,61 @@ public class EnumGenerator : IIncrementalGenerator
         context.RegisterSourceOutput(compilationAndEnums,
             static (spc, source) => Execute(source.Item1, source.Item2, spc));
     }
+
+    static bool IsSyntaxTargetForGeneration(SyntaxNode node)
+    => node is EnumDeclarationSyntax { AttributeLists.Count: > 0 };
+
+    private const string EnumExtensionsAttribute = "SG.EnumGenerators.EnumExtensionsAttribute";
+
+    static EnumDeclarationSyntax? GetSemanticTargetForGeneration(GeneratorSyntaxContext context)
+    {
+        // we know the node is a EnumDeclarationSyntax thanks to IsSyntaxTargetForGeneration
+        var enumDeclarationSyntax = (EnumDeclarationSyntax)context.Node;
+
+        // loop through all the attributes on the method
+        foreach (AttributeListSyntax attributeListSyntax in enumDeclarationSyntax.AttributeLists)
+        {
+            foreach (AttributeSyntax attributeSyntax in attributeListSyntax.Attributes)
+            {
+                if (context.SemanticModel.GetSymbolInfo(attributeSyntax).Symbol is not IMethodSymbol attributeSymbol)
+                {
+                    // weird, we couldn't get the symbol, ignore it
+                    continue;
+                }
+
+                INamedTypeSymbol attributeContainingTypeSymbol = attributeSymbol.ContainingType;
+                string fullName = attributeContainingTypeSymbol.ToDisplayString();
+
+                // Is the attribute the [EnumExtensions] attribute?
+                if (fullName == EnumExtensionsAttribute)
+                {
+                    // return the enum
+                    return enumDeclarationSyntax;
+                }
+            }
+        }
+
+        // we didn't find the attribute we were looking for
+        return null;
+    }
+
+    static void Execute(Compilation compilation, ImmutableArray<EnumDeclarationSyntax> enums, SourceProductionContext context)
+    {
+        if (enums.IsDefaultOrEmpty)
+            return;
+
+        IEnumerable<EnumDeclarationSyntax> distinctEnums = enums.Distinct();
+
+        // Convert each EnumDeclarationSyntax to an EnumToGenerate
+        List<EnumToGenerate> enumsToGenerate = GetTypesToGenerate(compilation, distinctEnums, context.CancellationToken);
+
+        // If there were errors in the EnumDeclarationSyntax, we won't create an
+        // EnumToGenerate for it, so make sure we have something to generate
+        if (enumsToGenerate.Count > 0)
+        {
+            // generate the source code and add it to the output
+            string result = SourceGenerationHelper.GenerateExtensionClass(enumsToGenerate);
+            context.AddSource("EnumExtensions.g.cs", SourceText.From(result, Encoding.UTF8));
+        }
+    }
 }
