ad8efba004ad5300af83860394eae00442da71e3
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-14 14:55:22 +0200

    Creating the incremental source generator

---
+++ src/SG.EnumGenerators/EnumGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace SG.EnumGenerators;
+[Generator]
+public class EnumGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // Add the marker attribute to the compilation
+        context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
+            "EnumExtensionsAttribute.g.cs",
+            SourceText.From(SourceGenerationHelper.Attribute, Encoding.UTF8)));
+
+        // TODO: implement the remainder of the source generator
+    }
+}
