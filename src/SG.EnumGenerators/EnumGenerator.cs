7f97da2d71304a1e534624a885116b7d5e132fa3
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-14 15:10:17 +0200

    Building incremental generator pipeline

--- src/SG.EnumGenerators/EnumGenerator.cs
+++ src/SG.EnumGenerators/EnumGenerator.cs
@@ -1,18 +1,34 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using System.Collections.Immutable;
 using System.Text;
 
 namespace SG.EnumGenerators;
+
 [Generator]
 public class EnumGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        // Add the marker attribute to the compilation
+        // Add the marker attribute
         context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
             "EnumExtensionsAttribute.g.cs",
             SourceText.From(SourceGenerationHelper.Attribute, Encoding.UTF8)));
 
-        // TODO: implement the remainder of the source generator
+        // Do a simple filter for enums
+        IncrementalValuesProvider<EnumDeclarationSyntax> enumDeclarations = context.SyntaxProvider
+            .CreateSyntaxProvider(
+                predicate: static (s, _) => IsSyntaxTargetForGeneration(s), // select enums with attributes
+                transform: static (ctx, _) => GetSemanticTargetForGeneration(ctx)) // sect the enum with the [EnumExtensions] attribute
+            .Where(static m => m is not null)!; // filter out attributed enums that we don't care about
+
+        // Combine the selected enums with the `Compilation`
+        IncrementalValueProvider<(Compilation, ImmutableArray<EnumDeclarationSyntax>)> compilationAndEnums
+            = context.CompilationProvider.Combine(enumDeclarations.Collect());
+
+        // Generate the source using the compilation and enums
+        context.RegisterSourceOutput(compilationAndEnums,
+            static (spc, source) => Execute(source.Item1, source.Item2, spc));
     }
 }
