6885395ddb937e7946a8d6cc0adb4004d2cc0865
Ruslan Rudenko <theruslan.prog@gmail.com>
2021-12-17 15:54:54 +0200

    ModuleInitializer must be added, so add one

---
+++ tests/NetEscapades.EnumGenerators.Tests/EnumGeneratorSnapshotTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+namespace SG.EnumGenerators.Tests
+{
+    [UsesVerify]
+    public class EnumGeneratorSnapshotTests
+    {
+        [Fact]
+        public Task GeneratesEnumExtensionsCorrectly()
+        {
+            var source = @"
+using SG.EnumGenerators;
+
+[EnumExtensions]
+public enum Color
+{
+    Red = 0,
+    Blue = 1,
+}";
+
+            return TestHelper.Verify(source);
+        }
+    }
+}
